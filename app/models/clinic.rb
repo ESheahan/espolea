b78fc057eb2d31a76aa587f14214b7fd9693c784
Ana Arellano <a010899966@itesm.mx>
2016-04-04 14:03:09 -0500

    Preparation for filters on clinics
    -Requiered fields
    -New filter search

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -2,7 +2,11 @@ class Clinic < ActiveRecord::Base
 
   belongs_to :user
   has_many :schedules
-
+  
+  #Required fields for possible filters
+  validates :name, presence: true
+  validates :state, presence: true
+  validates :municipality, presence: true
   #For state filter
   scope :by_state, lambda { |state|
     where("state LIKE ?", "%#{state}%")
@@ -10,6 +14,10 @@ class Clinic < ActiveRecord::Base
   #For municipality filter
   scope :by_municipality, lambda { |municipality|
     where("municipality LIKE ?", "%#{municipality}%")
+  }
+  #For name filter
+  scope :by_name, lambda { |name|
+    where("name LIKE ?", "%#{name}%")
   }
 
 end
