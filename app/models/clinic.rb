bb562f514e87af9c761a60097ebb63763d166140
Ana Arellano <a010899966@itesm.mx>
2016-04-04 18:30:05 -0500

    Clinics Filter
    -Implementation for the filter by state, name and municipality
    -A little formating of the layout
    -Migrate clinics for testing

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -2,11 +2,42 @@ class Clinic < ActiveRecord::Base
 
   belongs_to :user
   has_many :schedules
-  
+  filterrific(
+  available_filters: [
+    :search_query,
+    :by_state,
+    :by_municipality
+  ]
+  )
   #Required fields for possible filters
   validates :name, presence: true
   validates :state, presence: true
   validates :municipality, presence: true
+  scope :search_query, lambda { |query|
+    return nil  if query.blank?
+    # condition query, parse into individual keywords
+    terms = query.downcase.split(/\s+/)
+    # replace "*" with "%" for wildcard searches,
+    # append '%', remove duplicate '%'s
+    terms = terms.map { |e|
+      (e.gsub('*', '%') + '%').gsub(/%+/, '%')
+    }
+    # configure number of OR conditions for provision
+    # of interpolation arguments. Adjust this if you
+    # change the number of OR conditions.
+    num_or_conditions = 3
+    where(
+      terms.map {
+        or_clauses = [
+          "LOWER(clinics.name) LIKE ?",
+          "LOWER(clinics.state) LIKE ?",
+          "LOWER(clinics.municipality) LIKE ?"
+        ].join(' OR ')
+        "(#{ or_clauses })"
+      }.join(' AND '),
+      *terms.map { |e| [e] * num_or_conditions }.flatten
+    )
+  }
   #For state filter
   scope :by_state, lambda { |state|
     where("state LIKE ?", "%#{state}%")
@@ -19,5 +50,13 @@ class Clinic < ActiveRecord::Base
   scope :by_name, lambda { |name|
     where("name LIKE ?", "%#{name}%")
   }
-
+  scope :order_by_name, -> {
+    order(:name => :desc)
+  }
+ def self.options_for_select_state
+  order('LOWER(state)').group('state').map { |e| [e.state, e.state] }
+ end
+ def self.options_for_select_municipality
+  order('LOWER(municipality)').group('municipality').map { |e| [e.municipality, e.municipality] }
+ end
 end
