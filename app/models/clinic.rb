b0b8896f40a4693d63d86f96051ccfea81fe62f2
Ana Arellano <a010899966@itesm.mx>
2016-04-21 16:36:53 -0500

    Scope rearrange
    -  Re structure for cliinic's state and municipality scopes

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -58,10 +58,10 @@ class Clinic < ActiveRecord::Base
     order(:name => :desc)
   }
  def self.options_for_select_state
-  order('LOWER(state)').group('clinics.id', 'state').map { |e| [e.state, e.state] }
+  select("DISTINCT state").order('LOWER(state)').group('clinics.id', 'state').map { |e| [e.state, e.state] }
  end
  def self.options_for_select_municipality
-  order('LOWER(municipality)').group('clinics.id', 'municipality').map { |e| [e.municipality, e.municipality] }
+  select("DISTINCT municipality").order('LOWER(municipality)').group('clinics.id', 'municipality').map { |e| [e.municipality, e.municipality] }
  end
  
  extend Searchable(:state)
