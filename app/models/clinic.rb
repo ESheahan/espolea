923a3e7e45ce226da5b448e95e91f846c8f12901
Ana Arellano <a010899966@itesm.mx>
2016-04-22 22:01:31 -0500

    Second resolve

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -58,10 +58,10 @@ class Clinic < ActiveRecord::Base
     order(:name => :desc)
   }
  def self.options_for_select_state
-  order('LOWER(state)').group('clinics.id', 'state').map { |e| [e.state, e.state] }
+  order('LOWER(state)').group('clinics.id', 'clinics.state').map { |e| [e.state, e.state] }
  end
  def self.options_for_select_municipality
-  order('LOWER(municipality)').group('clinics.id', 'municipality').map { |e| [e.municipality, e.municipality] }
+  order('LOWER(municipality)').group('clinics.id', 'clinics.municipality').map { |e| [e.municipality, e.municipality] }
  end
  
  extend Searchable(:state)
